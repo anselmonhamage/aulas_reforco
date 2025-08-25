f18fb709d6a948fde5fcf4f06732ab0302f8ce8f
Anselmo Nhamage <nhamageanselmo@gmail.com>
2025-08-25 22:18:40 +0200

    Create EntradaDados.java

---
+++ Aula 02-3 - Variáveis e Operadores/Extra/EntradaDados.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class EntradaDados {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Digite um valor: ");
+		int num = entrada.nextInt();
+
+        // Raíz quadrada usando método pow(Número, Expoente)
+		double raizQuadrada = Math.pow(num, 0.5);
+		System.out.println(raizQuadrada);
+		
+        // Raíz quadrada usando método sqrt(Raíz Quadrada)
+		raizQuadrada = Math.sqrt(num) + 5;
+		System.out.print(raizQuadrada);
+
+        entrada.close();
+    }
+
+    
+}
